a8f7ee2819c908c5aeda572ea07f02ef8bf0116c
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2024-11-12 23:27:40 -0300

    feat: create createUser function

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,3 +4,17 @@ export const getUserByEmail = async (email: string) => {
   const user = await prisma.user.findFirst({ where: { email } });
   return user;
 };
+
+type Create = {
+  name: string;
+  email: string;
+};
+
+export const createUser = async (data: Create) => {
+  try {
+    const user = await prisma.user.create({ select: { id: true }, data });
+    return user;
+  } catch (error) {
+    return new Error('Ocorreu um erro interno.');
+  }
+};
